a0bab65abfe5d470af333ba9d7e82ba4c087dc5e
vazeraja <68131084+vazeraja@users.noreply.github.com>
2023-09-29 12:59:31 -0400

    demo art

---
+++ SceneMapSystem/Assets/_Sprites/Tiled/basic_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="basic_tileset" tilewidth="16" tileheight="16" tilecount="144" columns="16">
+ <image source="basic_tile.png" width="269" height="155"/>
+</tileset>
