136510519e652236e36150990325f7fa431a6004
vazeraja <68131084+vazeraja@users.noreply.github.com>
2023-10-25 23:34:02 -0400

    --- n.a

--- SceneMapSystem/Assets/_Scripts/LoadingScreen.cs
+++ SceneMapSystem/Assets/_Scripts/LoadingScreen.cs
@@ -1,40 +1,40 @@
 using System.Globalization;
-using MoreMountains.Tools;
+// using MoreMountains.Tools;
 using TNS.SceneSystem;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class LoadingScreen : MonoBehaviour
 {
-    [SerializeField] private Text _text;
-    public float RemapMin = 0f;
-    public float RemapMax = 100f;
-    public int NumberOfDecimals = 0;
-    
-    [SerializeField] private Image _image;
-    
-    private void Awake()
-    {
-        SceneManager.setInterpolatedProgressValue += SetImageProgress;
-        SceneManager.setInterpolatedProgressValue += SetTextProgress;
-    }
-
-    private void OnDestroy()
-    {
-        SceneManager.setInterpolatedProgressValue -=SetImageProgress;
-        SceneManager.setInterpolatedProgressValue -= SetTextProgress;
-    }
-    
-    private void SetImageProgress(float newValue)
-    {
-        _image.fillAmount = newValue;
-    }
-
-    private void SetTextProgress(float newValue)
-    {
-        float remappedValue = MMMaths.Remap(newValue, 0f, 1f, RemapMin, RemapMax);
-        float displayValue = MMMaths.RoundToDecimal(remappedValue, NumberOfDecimals);
-        _text.text = displayValue.ToString(CultureInfo.InvariantCulture);
-    }
-    
+    // [SerializeField] private Text _text;
+    // public float RemapMin = 0f;
+    // public float RemapMax = 100f;
+    // public int NumberOfDecimals = 0;
+    //
+    // [SerializeField] private Image _image;
+    //
+    // private void Awake()
+    // {
+    //     SceneManager.setInterpolatedProgressValue += SetImageProgress;
+    //     SceneManager.setInterpolatedProgressValue += SetTextProgress;
+    // }
+    //
+    // private void OnDestroy()
+    // {
+    //     SceneManager.setInterpolatedProgressValue -=SetImageProgress;
+    //     SceneManager.setInterpolatedProgressValue -= SetTextProgress;
+    // }
+    //
+    // private void SetImageProgress(float newValue)
+    // {
+    //     _image.fillAmount = newValue;
+    // }
+    //
+    // private void SetTextProgress(float newValue)
+    // {
+    //     float remappedValue = MMMaths.Remap(newValue, 0f, 1f, RemapMin, RemapMax);
+    //     float displayValue = MMMaths.RoundToDecimal(remappedValue, NumberOfDecimals);
+    //     _text.text = displayValue.ToString(CultureInfo.InvariantCulture);
+    // }
+    //
 }
